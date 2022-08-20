b881723de4aa9e9db73293545e3fb7efdc737d48
jitujagtap <56694040+jitujagtap@users.noreply.github.com>
2022-08-20 15:33:16 +0530

    Add files via upload

---
+++ hackerrank_1.py
@@ -0,0 +1,6 @@
+#I am not able to print on the next line, could you please help me here?
+input_string = input("Welcome to 30 Days of Code!")
+# Print a string literal saying "Hello, World." to stdout.
+print("Hello, World.")
+# TODO: Write a line of code here that prints the contents of input_string to stdout.
+print(input_string)
